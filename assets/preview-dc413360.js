bb87f3276410013f783fc28c2d9bd4f5b73ca8ee
Daniel Blanco <danielba.developer@gmail.com>
2023-08-21 00:34:30 -0400

    3th commit

---
+++ assets/preview-dc413360.js
@@ -0,0 +1,2 @@
+import"./index-61bf1805.js";import"./_commonjsHelpers-de833af9.js";const r={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{r as default};
+//# sourceMappingURL=preview-dc413360.js.map
